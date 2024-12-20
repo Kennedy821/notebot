561690730a6106863a3520a8d558bf07817e4968
tariroMashCasa <ucfntma@ucl.ac.uk>
2024-12-20 11:17:39 +0000

    removed the whole custom paypal code

--- pages/notebot_registration_form.py
+++ pages/notebot_registration_form.py
@@ -108,142 +108,142 @@ if password and confirm_password:
 
 
 # Get query parameters to check if the user is returning from PayPal
-params = st.query_params
-payment_id = params.get('paymentId', [None])[0]
-payer_id = params.get('PayerID', [None])[0]
+# params = st.query_params
+# payment_id = params.get('paymentId', [None])[0]
+# payer_id = params.get('PayerID', [None])[0]
 
-# Check if we're handling the payment execution step
-if payment_id and payer_id:
-    st.info("Processing your payment. Please wait...")
-    # Find and execute the payment
-    payment = paypalrestsdk.Payment.find(payment_id)
-    if payment.execute({"payer_id": payer_id}):
-        st.success("Payment completed successfully!")
-        # Retrieve payer's email
-        payer_info = payment.payer.payer_info
-        payer_email = payer_info.email
+# # Check if we're handling the payment execution step
+# if payment_id and payer_id:
+#     st.info("Processing your payment. Please wait...")
+#     # Find and execute the payment
+#     payment = paypalrestsdk.Payment.find(payment_id)
+#     if payment.execute({"payer_id": payer_id}):
+#         st.success("Payment completed successfully!")
+#         # Retrieve payer's email
+#         payer_info = payment.payer.payer_info
+#         payer_email = payer_info.email
 
-    else:
-        st.error("Payment execution failed.")
-        st.write(payment.error)
+#     else:
+#         st.error("Payment execution failed.")
+#         st.write(payment.error)
 
-else:
+# else:
 
-    # Display the payment initiation UI
-    st.write("Subscribe to access premium features.")
-    if st.button("Register"):
+# Display the payment initiation UI
+st.write("Subscribe to access premium features.")
+if st.button("Register"):
 
-        with tempfile.TemporaryDirectory() as temp_dir:
+    with tempfile.TemporaryDirectory() as temp_dir:
 
 
 
-            # upload registration data to user credentials database
+        # upload registration data to user credentials database
+        
+        credentials_df = pd.DataFrame([email,password]).T
+        credentials_df.columns = ["email","pw"]
+        credentials_df["hash_pw"] = credentials_df.pw.apply(lambda x: hash_password(x).decode())
+        credentials_df["token"] = credentials_df.email.apply(lambda x: generate_token(x))
+        credentials_df["registration_date"] = pd.to_datetime(formatted_date,format="%d-%m-%Y")
+
+        # credentials_df.drop(columns="pw",inplace=True)
+        # Create credentials object
+        credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
+
+        # Use the credentials to create a client
+        client = storage.Client(credentials=credentials)
+
+
+        # The bucket on GCS in which to write the CSV file
+        bucket = client.bucket('psil-app-backend-2')
+        # The name assigned to the CSV file on GCS
+        blob = bucket.blob('new_psil_user_registration.csv')
+
+        # Convert the DataFrame to a CSV string with a specified encoding
+        csv_string = credentials_df.to_csv(index=False, encoding='utf-8')
+
+        # Upload the CSV string to GCS
+        blob.upload_from_string(csv_string, 'text/csv')
+
+        # check to see if the email address is already registered to the site
+
+        existing_user_list = []
+
+        time.sleep(5)
+
+
+        check_if_user_is_existing_user()
+
+
+    # # Create the payment object
+    # payment = paypalrestsdk.Payment({
+    #     "intent": "sale",
+    #     "payer": {
+    #         "payment_method": "paypal"
+    #     },
+    #     "redirect_urls": {
+    #         "return_url": "https://psilproject.streamlit.app/psil_login",  # Replace with your app's URL
+    #         "cancel_url": "https://psilproject.streamlit.app/psil_registration_form"
+    #     },
+    #     "transactions": [{
+    #         "amount": {
+    #             "total": "0.01",
+    #             "currency": "GBP"
+    #         },
+    #         "description": "PSIL - music recommender app subscription"
+    #     }]
+    # })
+
+    # if payment.create():
+    #     st.success("Payment created successfully!")
+    #     # Extract approval URL
+    #     for link in payment.links:
+    #         if link.rel == "approval_url":
+    #             approval_url = str(link.href)
+    #             # Automatically redirect
+    #             st.markdown(f"<meta http-equiv='refresh' content='0; url={approval_url}'>", unsafe_allow_html=True)
+    # else:
+    #     st.error("Error creating payment")
+    #     st.write(payment.error)
+
+    # st.success("Payment created successfully!")
+
+# if st.button("Register"):
+#     with st.spinner("Processing your registration..."):
+#         with tempfile.TemporaryDirectory() as temp_dir:
+
+
+
+#             # upload registration data to user credentials database
             
-            credentials_df = pd.DataFrame([email,password]).T
-            credentials_df.columns = ["email","pw"]
-            credentials_df["hash_pw"] = credentials_df.pw.apply(lambda x: hash_password(x).decode())
-            credentials_df["token"] = credentials_df.email.apply(lambda x: generate_token(x))
-            credentials_df["registration_date"] = pd.to_datetime(formatted_date,format="%d-%m-%Y")
+#             credentials_df = pd.DataFrame([email,password]).T
+#             credentials_df.columns = ["email","pw"]
+#             credentials_df["hash_pw"] = credentials_df.pw.apply(lambda x: hash_password(x).decode())
+#             credentials_df["token"] = credentials_df.email.apply(lambda x: generate_token(x))
 
-            # credentials_df.drop(columns="pw",inplace=True)
-            # Create credentials object
-            credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
+#             # credentials_df.drop(columns="pw",inplace=True)
+#             # Create credentials object
+#             credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
 
-            # Use the credentials to create a client
-            client = storage.Client(credentials=credentials)
-
-
-            # The bucket on GCS in which to write the CSV file
-            bucket = client.bucket('psil-app-backend-2')
-            # The name assigned to the CSV file on GCS
-            blob = bucket.blob('new_psil_user_registration.csv')
-
-            # Convert the DataFrame to a CSV string with a specified encoding
-            csv_string = credentials_df.to_csv(index=False, encoding='utf-8')
-
-            # Upload the CSV string to GCS
-            blob.upload_from_string(csv_string, 'text/csv')
-
-            # check to see if the email address is already registered to the site
-
-            existing_user_list = []
-
-            time.sleep(5)
+#             # Use the credentials to create a client
+#             client = storage.Client(credentials=credentials)
 
 
-            check_if_user_is_existing_user()
+#             # The bucket on GCS in which to write the CSV file
+#             bucket = client.bucket('psil-app-backend-2')
+#             # The name assigned to the CSV file on GCS
+#             blob = bucket.blob('new_psil_user_registration.csv')
 
+#             # Convert the DataFrame to a CSV string with a specified encoding
+#             csv_string = credentials_df.to_csv(index=False, encoding='utf-8')
 
-        # # Create the payment object
-        # payment = paypalrestsdk.Payment({
-        #     "intent": "sale",
-        #     "payer": {
-        #         "payment_method": "paypal"
-        #     },
-        #     "redirect_urls": {
-        #         "return_url": "https://psilproject.streamlit.app/psil_login",  # Replace with your app's URL
-        #         "cancel_url": "https://psilproject.streamlit.app/psil_registration_form"
-        #     },
-        #     "transactions": [{
-        #         "amount": {
-        #             "total": "0.01",
-        #             "currency": "GBP"
-        #         },
-        #         "description": "PSIL - music recommender app subscription"
-        #     }]
-        # })
+#             # Upload the CSV string to GCS
+#             blob.upload_from_string(csv_string, 'text/csv')
 
-        # if payment.create():
-        #     st.success("Payment created successfully!")
-        #     # Extract approval URL
-        #     for link in payment.links:
-        #         if link.rel == "approval_url":
-        #             approval_url = str(link.href)
-        #             # Automatically redirect
-        #             st.markdown(f"<meta http-equiv='refresh' content='0; url={approval_url}'>", unsafe_allow_html=True)
-        # else:
-        #     st.error("Error creating payment")
-        #     st.write(payment.error)
+#             # check to see if the email address is already registered to the site
 
-        # st.success("Payment created successfully!")
+#             existing_user_list = []
 
-    # if st.button("Register"):
-    #     with st.spinner("Processing your registration..."):
-    #         with tempfile.TemporaryDirectory() as temp_dir:
-
-
-
-    #             # upload registration data to user credentials database
-                
-    #             credentials_df = pd.DataFrame([email,password]).T
-    #             credentials_df.columns = ["email","pw"]
-    #             credentials_df["hash_pw"] = credentials_df.pw.apply(lambda x: hash_password(x).decode())
-    #             credentials_df["token"] = credentials_df.email.apply(lambda x: generate_token(x))
-
-    #             # credentials_df.drop(columns="pw",inplace=True)
-    #             # Create credentials object
-    #             credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
-
-    #             # Use the credentials to create a client
-    #             client = storage.Client(credentials=credentials)
-
-
-    #             # The bucket on GCS in which to write the CSV file
-    #             bucket = client.bucket('psil-app-backend-2')
-    #             # The name assigned to the CSV file on GCS
-    #             blob = bucket.blob('new_psil_user_registration.csv')
-
-    #             # Convert the DataFrame to a CSV string with a specified encoding
-    #             csv_string = credentials_df.to_csv(index=False, encoding='utf-8')
-
-    #             # Upload the CSV string to GCS
-    #             blob.upload_from_string(csv_string, 'text/csv')
-
-    #             # check to see if the email address is already registered to the site
-
-    #             existing_user_list = []
-
-                # check_if_user_is_existing_user()
+            # check_if_user_is_existing_user()
 
 
 
@@ -253,17 +253,17 @@ else:
 # if 'cancel' in params:
 #     st.warning("Payment was cancelled.")
 
-            # # this was the old code before which would direct the user to click the button on the webpage it
-            # time.sleep(5)
-            st.success("Registration successful!")
-            st.markdown("To access your account make a payment using the 'Subscribe' button below")
-            st.markdown("\n After we've received your payment you will get a link to the PSIL application in your email.")
-            st.markdown("Happy listening!")
+        # # this was the old code before which would direct the user to click the button on the webpage it
+        # time.sleep(5)
+        st.success("Registration successful!")
+        st.markdown("To access your account make a payment using the 'Subscribe' button below")
+        st.markdown("\n After we've received your payment you will get a link to the PSIL application in your email.")
+        st.markdown("Happy listening!")
 
-            # # Redirect to external site with the token as a query parameter
-            # redirect_url = f"https://psilproject.streamlit.app/psil_login"
-            # st.markdown(f"""
-            # <meta http-equiv="refresh" content="0; url={redirect_url}">
-            # """, unsafe_allow_html=True)
+        # # Redirect to external site with the token as a query parameter
+        # redirect_url = f"https://psilproject.streamlit.app/psil_login"
+        # st.markdown(f"""
+        # <meta http-equiv="refresh" content="0; url={redirect_url}">
+        # """, unsafe_allow_html=True)
 
 
